0cbc9f666628f7cabbc19b2b2f59e36f7e03836e
andiksetyawan <andik.setyawan19@gmail.com>
2021-06-08 11:14:39 +0700

    download temp file to reader stream, dynamic path filename

--- main.go
+++ main.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	"context"
-	"github.com/gorilla/mux"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
-	"time"
+	"strings"
 )
 
-func main()  {
+func main() {
 	endpoint := os.Getenv("MINIO_ENDPOINT")
 	accessKeyID := os.Getenv("MINIO_ACCESS_KEY_ID")
 	secretAccessKey := os.Getenv("MINIO_SECRET_ACCESS_KEY_ID")
-	useSSL,_ := strconv.ParseBool(os.Getenv("MINIO_SECURE"))
+	useSSL, _ := strconv.ParseBool(os.Getenv("MINIO_SECURE"))
 
 	minioClient, err := minio.New(endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
@@ -28,32 +27,32 @@ func main()  {
 		panic(err)
 	}
 
-	r := mux.NewRouter()
-	r.HandleFunc("/{bucket}/{id}", func (w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		paths := strings.Split(r.URL.Path, "/")
+		bucket := paths[1]
+		fileName := paths[len(paths)-1]
+		fileFullPath := strings.Join(paths[2:], "/")
 
-		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
-		defer cancel()
-
-		object, err := minioClient.GetObject(ctx, vars["bucket"], vars["id"], minio.GetObjectOptions{})
+		object, err := minioClient.GetObject(context.Background(), bucket, fileFullPath, minio.GetObjectOptions{})
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 			return
 		}
+		defer object.Close()
 
-		b, err := ioutil.ReadAll(object)
+		w.Header().Set("Content-Disposition", "attachment; filename="+fileName)
+		_, err = io.Copy(w, object)
 		if err != nil {
 			log.Println(err)
+			w.Header().Del("Content-Disposition")
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 			return
 		}
-		w.Write(b)
 	})
 
 	log.Println("server started at :8080")
-	panic(http.ListenAndServe(":8080", r))
+	panic(http.ListenAndServe(":8080", nil))
 }
-
